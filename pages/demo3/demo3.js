6d4be82a77cc030319c0a5b3b7b000affced23d4
zhengjiong <zhengjiong2006@gmail.com>
2017-05-21 21:26:33 +0800

    fix<demo3>update style

--- pages/demo3/demo3.js
+++ pages/demo3/demo3.js
@@ -6,13 +6,59 @@ Page({
    */
   data: {
     tabs: [
-      'A','B','C','D','E','F','G','H','I'
+      'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'
     ],
     banners: [
       '/images/banner_1.jpg',
       '/images/banner_2.jpg',
       '/images/banner_3.jpg',
       '/images/banner_4.jpg',
+    ],
+    scrollItems: [
+      {
+        url: '/images/banner_1.jpg',
+        title: 'banner1'
+      },
+      {
+        url: '/images/banner_2.jpg',
+        title: 'banner2'
+      },
+      {
+        url: '/images/banner_3.jpg',
+        title: 'banner3'
+      },
+      {
+        url: '/images/banner_4.jpg',
+        title: 'banner4'
+      }
+    ],
+    infoItems: [
+      {
+        url: '/images/banner_1.jpg',
+        title: "title1",
+        subTitle: "subTitle1",
+        score: "100分"
+      },
+      {
+        url: '/images/banner_2.jpg',
+        title: "title2",
+        subTitle: "subTitle2",
+        score: "100分"
+      },
+      {
+        url: '/images/banner_3.jpg',
+        title: "title3",
+        subTitle: "subTitle3",
+        score: "100分"
+      },
+      {
+        url: '/images/banner_4.jpg',
+        title: "title4",
+        subTitle: "subTitle4",
+        score: "100分"
+      }
+
+
     ]
   },
 
@@ -20,55 +66,55 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
